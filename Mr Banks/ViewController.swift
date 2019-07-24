d5b16f46e2832940e055111f675d60faefd48183
Chloe <chloe_koh_hui_ru@s2018.ssts.edu.sg>
2019-07-24 15:34:40 +0800

    initial commit

---
+++ Mr Banks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mr Banks
+//
+//  Created by Chloe Koh on 24/7/19.
+//  Copyright © 2019 Chloe Koh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
